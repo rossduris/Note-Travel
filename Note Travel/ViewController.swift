f4222bb074c7069d17e1548adbadfcb1a1b4b2b7
Ross <ross@duris.io>
2016-01-06 19:25:58 -0500

    Initial Commit

---
+++ Note Travel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Note Travel
+//
+//  Created by Ross Duris on 1/6/16.
+//  Copyright © 2016 duris.io. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
